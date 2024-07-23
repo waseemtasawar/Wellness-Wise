ca1020d38edabff5e877b611afda69a4fcc99c76
waseem tasawar <waseemtasawar97@gmail.com>
2024-07-23 12:24:27 +0500

    Add mew Component and Data

---
+++ src/Data.js
@@ -0,0 +1,32 @@
+import picture1 from "./assets/w1.jpg";
+import picture2 from "./assets/w2.jpg";
+import picture3 from "./assets/w3.jpeg";
+export default [
+  {
+    id: "q1",
+    heading: "General-Check-up",
+    text: "Comprehensive heathcare checkup to ensure you are well bring",
+    image: {
+      src: picture1,
+      alt: "heathcare",
+    },
+  },
+  {
+    id: "q1",
+    heading: "Emergemcy-care",
+    text: "24/7 emergency service of critical heath situation",
+    image: {
+      src: picture2,
+      alt: "heathcare",
+    },
+  },
+  {
+    id: "q1",
+    heading: "Specialist Consultatns",
+    text: "Consultatns with top Specialist in verious filed of medicine",
+    image: {
+      src: picture3,
+      alt: "heathcare",
+    },
+  },
+];
--- src/component/pages/Home.jsx
+++ src/component/pages/Home.jsx
@@ -1,27 +1,51 @@
 import React from "react";
 import healthCare from "../../assets/healthcare.jpg";
+import Data from "../../Data.js";
 const Home = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-center items-center bg-blue-400">
-      <div className="text-center md:text-left md:mr-12 mb-6 md:mb-0 ">
-        <h1 className="text-4xl md:text-5xl mt-5 antialiased font-sans font-bold text-white pb-5 ">
-          Your Health, Our Priority
-        </h1>
-        <p className="text-base md:text-xl font-sans text-white">
-          provide world-class heathcare solution with compassion and excellence
-        </p>
-        <button className="font-sans text-white bg-blue-700 p-2 mt-3 rounded-lg border-none hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-600">
-          Explore Services
-        </button>
-      </div>
-      <div className=" justify-between items-center ">
-        <img
-          className="h-52 md:h-80 w-full md:w-auto p-8 my-8 object-cover rounded-full border-4 border-blue-500 shadow-lg "
-          src={healthCare}
-          alt="Heath Care"
-        />
-      </div>
-    </div>
+    <>
+      <section className="flex flex-col md:flex-row justify-center items-center bg-blue-400">
+        <div className="text-center md:text-left md:mr-12 mb-6 md:mb-0 ">
+          <h1 className="text-4xl md:text-5xl mt-5 antialiased font-sans font-bold text-white pb-5 ">
+            Your Health, Our Priority
+          </h1>
+          <p className="text-base md:text-xl font-sans text-white">
+            provide world-class heathcare solution with compassion and
+            excellence
+          </p>
+          <button className="font-sans text-white bg-blue-700 p-2 mt-3 rounded-lg border-none hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-600">
+            Explore Services
+          </button>
+        </div>
+        <div className=" justify-between items-center ">
+          <img
+            className="h-52 md:h-80 w-full md:w-auto p-8 my-8 object-cover rounded-full border-4 border-blue-500 shadow-lg "
+            src={healthCare}
+            alt="Heath Care"
+          />
+        </div>
+      </section>
+      <section>
+        <div>
+          <h1>Our Services</h1>
+          <p>
+            we Provide a wide range of healthcare services which meet all your
+            needs
+          </p>
+        </div>
+        <div>
+          <ul>
+            {Data.map((item) => (
+              <li key={item.id}>
+                <h1>{item.heading}</h1>
+                <p> {item.text} </p>
+                <img src={item.image.src} alt={item.image.alt} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+    </>
   );
 };
 
